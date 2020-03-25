ddfcca4b973f21db03ea08535f8e71f119feef36
Mohamed Osman <mohamed.osman.dev@gmail.com>
2020-03-25 16:15:12 +0100

    Update project_code_ino_ino.ino

--- project_code/project_code_ino/project_code_ino_ino/project_code_ino_ino.ino
+++ project_code/project_code_ino/project_code_ino_ino/project_code_ino_ino.ino
@@ -3,7 +3,7 @@
 int waterPump = A0; 
 String voice;
   int sensorValue;
- 
+
 // the setup routine runs once when you press reset:
 void setup() {
   // initialize serial communication at 9600 bits per second:
@@ -17,17 +17,18 @@ void setup() {
 void loop() {
  
   sensorValue = analogRead(A4);
-  
   while (Serial.available()){  //Check if there is an available byte to read
-  delay(10); //Delay added to make thing stable
-  char c = Serial.read(); //Conduct a serial read
-  if (c == '#') {break;} //Exit the loop when the # is detected after the word
-  voice += c; //Shorthand for voice = voice + c
-   sensorValue = analogRead(A4);
-  
-  Serial.println(sensorValue);
-  delay(100);        // delay in between reads for stability
-     }
+    delay(10); //Delay added to make thing stable
+    char c = Serial.read(); //Conduct a serial read
+    //Exit the loop when the # is detected after the wordeak;
+    if (c == '#'){
+      break;
+    } 
+    voice += c; //Shorthand for voice = voice + c
+    sensorValue = analogRead(A4);
+    Serial.println(sensorValue);
+    delay(100);        // delay in between reads for stability
+  }
   
   Serial.println(sensorValue);
   delay(100);
@@ -39,24 +40,23 @@ void loop() {
 if (voice.length() > 0) {
     Serial.println(voice);
  
-  if(voice == "*start")
-  {
+   if(voice == "*start"){
     digitalWrite(waterPump, HIGH);
   }
    
-   else if(voice == "*trigga")
-   {
+   else if(voice == "*trigga"){
      digitalWrite(RELAY1, 1);
    }
-   else  if(voice == "*avbryt")
-   {
+   else  if(voice == "*avbryt"){
      digitalWrite(RELAY1, 0);
    digitalWrite(waterPump,LOW);
-   }
+  }
   
- 
-voice="";}} //Reset the variable after initiating
+  //Reset the variable after initiating
+  voice="";
+}
 
+}
          
          
   
